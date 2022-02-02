1c3f9300d1acb035aca9631c76d42a9236d7b0d6
Juan Antonio Zamora Arosemena <36899684+juanzeta33@users.noreply.github.com>
2022-02-02 11:57:24 -0500

    Create Fruta.java
    Subiendo entidad fruta

---
+++ Fruta.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author jotaz
+ */
+public class Fruta {
+
+    /**
+     * @return the creado_por
+     */
+    public int getCreado_por() {
+        return creado_por;
+    }
+
+    /**
+     * @param creado_por the creado_por to set
+     */
+    public void setCreado_por(int creado_por) {
+        this.creado_por = creado_por;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the procedencia
+     */
+    public String getProcedencia() {
+        return procedencia;
+    }
+
+    /**
+     * @param procedencia the procedencia to set
+     */
+    public void setProcedencia(String procedencia) {
+        this.procedencia = procedencia;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the created_at
+     */
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    /**
+     * @param created_at the created_at to set
+     */
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    /**
+     * @return the updated_at
+     */
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    /**
+     * @param updated_at the updated_at to set
+     */
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+    private int id;
+    private String nombre;
+    private String procedencia;
+    private String categoria;
+    private int creado_por;
+    private String created_at;
+    private String updated_at;
+}
